5a9d9a57daab69f0eb8a967ea8535a82220a9ecf
Goran Gajic <fero.null@gmail.com>
2012-05-25 21:54:22 +0200

    hello

---
+++ slagalica.user.js
@@ -0,0 +1,20 @@
+// Ludara.com - otklananje
+// version 0.1
+// Copyright (c) 2011, ludara.com
+// ==UserScript==
+// @name          Ludara.com
+// @namespace     http://www.ludara.com/asocijacije
+// @description   Otklanjanje 
+// @include       http://fb.slagalica-online.com/application/*
+// @include       https://fb.slagalica-online.com/application/*
+// ==/UserScript==
+
+
+    (function(){
+            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+                GM_Script = document.createElement('script');
+            GM_Script.src = 'https://raw.github.com/feroc1ty/vukajlija-script/master/beautiful-vukajlija.js';
+            GM_Script.type = 'text/javascript';
+            GM_Script.async = true;
+            GM_Head.insertBefore(GM_Script, GM_Head.firstChild);
+	})();
