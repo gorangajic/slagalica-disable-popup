5aaa3e8024228d19fed29aefaafad204e8f06c74
Goran Gajic <fero.null@gmail.com>
2012-05-25 21:56:06 +0200

    hello

--- slagalica.user.js
+++ slagalica.user.js
@@ -13,7 +13,7 @@
     (function(){
             var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
                 GM_Script = document.createElement('script');
-            GM_Script.src = 'https://raw.github.com/feroc1ty/vukajlija-script/master/beautiful-vukajlija.js';
+            GM_Script.src = 'https://raw.github.com/feroc1ty/slagalica-disable-popup/master/slagalica.js';
             GM_Script.type = 'text/javascript';
             GM_Script.async = true;
             GM_Head.insertBefore(GM_Script, GM_Head.firstChild);
