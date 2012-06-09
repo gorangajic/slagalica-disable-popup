095a903e33c6e8e1bafe31eb83992604e50ae688
Goran Gajic <fero.null@gmail.com>
2012-06-09 16:15:45 +0200

    hello

--- slagalica.js
+++ slagalica.js
@@ -1,5 +1,5 @@
 setTimeout(function() {
   blurString = window.onblur.toString();
-  window.checkCodeBody() { if(bIsMSIE) {document.getElementById('flashClient').checkCode(blurString)} else {document.getElementById('flashClient').checkCode(blurString)} };
+  window.checkCodeBody = function() { if(bIsMSIE) {document.getElementById('flashClient').checkCode(blurString)} else {document.getElementById('flashClient').checkCode(blurString)} };
   window.onblur = null;
 },3000);
